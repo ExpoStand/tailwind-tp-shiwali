e58b34c8fcf8b58e67b8f088a02c314811bdd1ec
ExpoStand <karlceller@gmail.com>
2024-06-05 18:41:05 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import dynamic from "next/dynamic";
 
 export default function Home() {
-  const HomePageForm = dynamic(() => import('@/components/homePageForm'));
+
   return (
    <>
-   <HomePageForm />
+
    </>
   );
 }
